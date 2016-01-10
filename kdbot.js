26657374e24dcb39c8c08296c119505425f68ca6
aspin <tsunami70875@gmail.com>
2016-01-10 12:55:15 -0600

    Added plugins for complimenting and insulting

--- kdbot.js
+++ kdbot.js
@@ -1,8 +1,11 @@
 'use strict';
 
-let Botkit = require('botkit');
+let Botkit = require('botkit'),
+    compliments = require('./compliments.js'),
+    insults = require('./insults.js');
+
 let controller = Botkit.slackbot();
-let bot = controller.spawn({
+controller.spawn({
   token: 'xoxb-18080744019-faM0j4W463WjwykawSxvTh9y'
 }).startRTM((err, bot, payload) => {
   if (err) {
@@ -10,6 +13,28 @@ let bot = controller.spawn({
   }
 });
 
-controller.hears(['hi'], ['direct_message', 'ambient', 'mention'], function(bot, message) {
+controller.hears(['hi'], ['direct_message', 'ambient', 'direct_mention'], (bot, message) => {
   bot.reply(message, 'Looks like Heroku\'s back up!');
+});
+
+controller.hears(['compliment me'], ['direct_message', 'direct_mention'], (bot, message) => {
+  bot.reply(message, selectRandomFromArray(compliments));
+});
+
+controller.hears(['insult me'], ['direct_message', 'direct_mention'], (bot, message) => {
+  bot.reply(message, selectRandomFromArray(insults));
+});
+
+function selectRandomFromArray(array) {
+  let index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
+
+// TODO: Format this better and make it automatic?
+controller.hears(['help'], ['direct_message', 'direct_mention'], (bot, message) => {
+  let helptext = 'I can do the following: \n\
+    - compliment me: I can give you a self esteem boost! \n\
+    - insult me: I can lower your self esteem! \n\
+  ';
+  bot.reply(message, helptext);
 });
